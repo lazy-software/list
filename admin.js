5c4fb95b315687999d8902b7c9b2fb309e64eccc
Brian Bowman <bowmanb@gmail.com>
2025-11-19 22:58:37 -0500

    v2 rebuild

--- admin.js
+++
@@ -1,6 +0,0 @@
-window.onload = (_event) => {
-    const destroyButton = document.querySelector("#destroyButton");
-    destroyButton.onclick = function () {
-        localStorage.clear();
-    };
-};
